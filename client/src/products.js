e1e65dce44a4e6da1a54d6c30fea46da8355bfed
Peupty <melvillewatford@gmail.com>
2020-03-10 18:49:40 +0100

    products added

---
+++ client/src/products.js
@@ -0,0 +1,112 @@
+export default [
+  {
+    name: "Wienerberger Porotherm P+W 25 kl. 15",
+    price: "6,58 zł brutt/szt",
+    img: "/assets/1.png",
+    description: `Pustak ceramiczny przeznaczony do budowy zewnętrznych
+    ścian nośnych z dociepleniem oraz ścian nośnych wewnętrznych. 
+    Grubość pustaków i ścian wynosi 25 cm. 
+    Porotherm 25 P+W muruje się na “pióro wpust”, 
+    dzięki czemu nie jest konieczne stosowanie spoin pionowych, 
+    co znacznie przyspiesza prowadzenie prac budowlanych i poprawia 
+    parametry cieplne muru.`,
+    advantages: [
+      "wytrzymałość muru aż do 6,5 MPa (wytrzymałość charakterystyczna na ściskanie)",
+      "możliwość zastosowania na ścianę nośną zewnętrzną i wewnętrzną",
+      "wysoka izolacyjność akustyczna",
+      "wysoka trwałość",
+      "ekologiczny, naturalny materiał"
+    ]
+  },
+  {
+    name: "Wienerberger Dryfix P+W 25 kl. 15",
+    price: "10,45 zł brutto/szt",
+    img: "/assets/1-1.png",
+    description:
+      "Pustak szlifowany przeznaczony do budowy zewnętrznych ścian nośnych z dociepleniem oraz ścian nośnych wewnętrznych. Murowany na cienko spoinową zaprawę do murowania na sucho Porotherm Dryfix, której zastosowanie znacznie przyspiesza prowadzenie prac budowlanych i poprawia parametry cieplne muru. Grubość ściany: 25 cm.",
+    advantages: [
+      "wytrzymałość muru aż do 6,5 MPa (wytrzymałość charakterystyczna na ściskanie)",
+      "możliwość zastosowania na ścianę nośną zewnętrzną i wewnętrzną",
+      "wysoka izolacyjność akustyczna",
+      "wysoka trwałość",
+      "ekologiczny, naturalny materiał",
+      "niższy koszt budowy",
+      "zerowe zapotrzebowanie na zaprawę, wodę oraz prąd",
+      "zmniejszone zapotrzebowanie na tynk ze względu na dokładność wymiarów muru",
+      "konstrukcja oszczędzająca wodę i materiały skutkuje znacznie zmniejszonym obciążeniem środowiska, tworząc w ten sposób konstrukcję bardziej przyjazną dla środowiska",
+      "niższe koszty pracy",
+      "praca jest czystsza, wydajniejsza, lepiej zorganizowana, ze znacznie krótszym czasem murowania",
+      "szybsze suszenie, osiąga pełne właściwości po 24 godzinach"
+    ]
+  },
+  {
+    name: "Leier Thermopor N+F 25 kl. 15",
+    price: "5,95 zł brutto/szt",
+    img: "/assets/1-2.png",
+    description:
+      "Pustaki do wznoszenia ścian zewnętrznych z dociepleniem (dwu- i trójwarstwowych) i wewnętrznych, ściany nośne, samonośne, i wypełniające w budownictwie mieszkaniowym, przemysłowym i użyteczności publicznej.",
+    advantages: [
+      "wytrzymałość muru aż do 6,5 MPa (wytrzymałość charakterystyczna na ściskanie)",
+      "możliwość zastosowania na ścianę nośną zewnętrzną i wewnętrzną",
+      "wysoka izolacyjność akustyczna",
+      "wysoka trwałość",
+      "ekologiczny, naturalny materiał"
+    ]
+  },
+  {
+    name: "Leier PLAN N+F 25 kl. 15",
+    price: "7,00 zł brutto/szt",
+    img: "/assets/1-3.png",
+    description:
+      "Leier PLAN 25 szlifowany pustak ceramiczny klejony na dedykowaną pianę Leier FIX, lub zaprawę cienko spoinową, której zastosowanie znacznie przyspiesza prowadzenie prac budowlanych i poprawia parametry cieplne muru. Grubość ściany to 25 cm.",
+    advantages: [
+      "wytrzymałość muru aż do 6,5 MPa (wytrzymałość charakterystyczna na ściskanie)",
+      "możliwość zastosowania na ścianę nośną zewnętrzną i wewnętrzną",
+      "wysoka izolacyjność akustyczna",
+      "wysoka trwałość",
+      "ekologiczny, naturalny materiał",
+      "niższy koszt budowy",
+      "zerowe zapotrzebowanie na zaprawę, wodę oraz prąd",
+      "zmniejszone zapotrzebowanie na tynk ze względu na dokładność wymiarów muru",
+      "konstrukcja oszczędzająca wodę i materiały skutkuje znacznie zmniejszonym obciążeniem środowiska, tworząc w ten sposób konstrukcję bardziej przyjazną dla środowiska",
+      "niższe koszty pracy",
+      "praca jest czystsza, wydajniejsza, lepiej zorganizowana, ze znacznie krótszym czasem murowania",
+      "szybsze suszenie, osiąga pełne właściwości po 24 godzinach"
+    ]
+  },
+  {
+    name: "XPS Ravatherm",
+    price: "387,45 zł brutto/m3",
+    img: "/assets/1-4.png",
+    advantages: [
+      -"Długowieczność utrzymania własności izolacyjnych",
+      "Odporność na wodę oraz zamarzanie",
+      "Wysoka sztywność",
+      "Stabilność wymiarowa",
+      "Odporność na gnicie i rozpad",
+      "Łatwa i szybka instalacja z małą ilością odpadów"
+    ],
+    description:
+      "Polistyrenu ekstrudowany RAVATHERM™ XPS-G 300 SL o strukturze zamknięto komórkowej."
+  },
+  {
+    name: "Płyta OSB Kronopol OSB 3",
+    price: "1228 zł brutto/m3",
+    img: "/assets/1-5.png"
+  },
+  {
+    name: "Stal zbrojeniowa",
+    price: "2704 zł brutto/tona",
+    img: "/assets/1-6.png"
+  },
+  {
+    name: "System elewacji Arsanit",
+    price: "64,99 brutto zł/m3",
+    img: "/assets/1-7.png"
+  },
+  {
+    name: "Styropiany Arsanit",
+    price: "64,99 brutto zł/m3",
+    img: "/assets/1-8.png"
+  }
+];
