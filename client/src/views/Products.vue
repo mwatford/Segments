5066b9a30478205d0373d90972b711903375fcca
Peupty <melvillewatford@gmail.com>
2020-03-10 18:53:04 +0100

    created products section with basic styling

---
+++ client/src/views/Products.vue
@@ -0,0 +1,57 @@
+<template>
+  <section id="produkty" class="col">
+    <h1>Nasze produkty.</h1>
+    <ul>
+      <li v-for="(item, index) in products" :key="index">
+        <product :item="item"></product>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import product from "../components/product-tile.vue";
+import products from "../products";
+
+export default {
+  components: {
+    product
+  },
+  data() {
+    return {
+      products
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+section {
+  padding-top: 40px;
+  align-items: center;
+}
+h1 {
+  font-family: futura-pt, sans-serif;
+  margin: auto;
+  font-size: 80px;
+  margin-bottom: 100px;
+  font-weight: 700;
+  color: var(--header);
+}
+ul {
+  @media (max-width: 1000px) {
+    width: 100%;
+    justify-content: space-evenly;
+  }
+  display: flex;
+  list-style-type: none;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  width: 1398px;
+
+  li {
+    min-width: 446px;
+  }
+
+}
+</style>
