5f643e2d11eb61bdf1f92914d51d9889f0ccd495
Peupty <melvillewatford@gmail.com>
2020-03-11 23:53:15 +0100

    moved header class to be global

--- client/src/App.vue
+++ client/src/App.vue
@@ -18,6 +18,27 @@ body {
   background-color: #fff;
   scroll-behavior: smooth;
 }
+.header {
+  width: 100%;
+  max-width: 930px;
+  margin-left: 300px;
+  user-select: none;
+
+  h1 {
+    margin-bottom: 40px;
+    font-size: 80px;
+    font-weight: 700;
+    letter-spacing: 0;
+    color: var(--header);
+  }
+  p {
+    font-family: museo-slab;
+    font-size: 24px;
+    line-height: 32px;
+    letter-spacing: 0;
+    font-weight: 300;
+  }
+}
 :root {
   --pink: #ef529d;
   --yellow: #fbb040;
