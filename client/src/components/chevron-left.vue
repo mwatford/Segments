8d97515ede24925d797ad71855b708acf1e8d141
Peupty <melvillewatford@gmail.com>
2020-03-13 20:47:20 +0100

    created icon

---
+++ client/src/components/chevron-left.vue
@@ -0,0 +1,70 @@
+<template>
+  <svg
+    id="Layer_1"
+    data-name="Layer 1"
+    xmlns="http://www.w3.org/2000/svg"
+    width="48"
+    height="48"
+    viewBox="0 0 3097.47 2519.97"
+  >
+    <title>wroc</title>
+    <g id="Group_133" data-name="Group 133">
+      <g id="chevron-left">
+        <g id="chevron-left-2">
+          <g id="chevron-left-3">
+            <rect
+              id="Rectangle_1071"
+              data-name="Rectangle 1071"
+              class="cls-1"
+              width="2519.97"
+              height="2519.97"
+            />
+            <path
+              id="Path_1809"
+              data-name="Path 1809"
+              class="cls-2"
+              d="M554.05,1224.78a105,105,0,0,1-75.6-32.55l-405.29-420a105,105,0,0,1,0-147l420-420a105.43,105.43,0,1,1,149.1,149.09L295.75,699.79l333.9,346.5a104.47,104.47,0,0,1-75.6,178.49Z"
+              transform="translate(848.73 559.99)"
+            />
+          </g>
+        </g>
+      </g>
+      <g id="chevron-left-4">
+        <g id="chevron-left-5">
+          <g id="chevron-left-6">
+            <rect
+              id="Rectangle_1071-2"
+              data-name="Rectangle 1071-2"
+              class="cls-1"
+              x="577.49"
+              width="2519.97"
+              height="2519.97"
+            />
+            <path
+              id="Path_1809-2"
+              data-name="Path 1809-2"
+              class="cls-2"
+              d="M1131.55,1224.78a105,105,0,0,1-75.6-32.55l-405.3-420a105,105,0,0,1,0-147l420-420a105.42,105.42,0,1,1,149.09,149.09L873.25,699.79l333.89,346.5a104.47,104.47,0,0,1-75.59,178.49Z"
+              transform="translate(848.73 559.99)"
+            />
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.cls-1,
+.cls-2 {
+  fill: #fff;
+}
+
+.cls-1 {
+  fill-opacity: 0;
+}
+</style>
