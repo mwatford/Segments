e779f84dc9a6e5ee56f2295629d7a3264f86880b
Peupty <melvillewatford@gmail.com>
2020-03-13 20:46:54 +0100

    created app button

---
+++ client/src/components/button.vue
@@ -0,0 +1,36 @@
+<template>
+  <button>
+    <chevron-left></chevron-left>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+import chevronLeft from "./chevron-left.vue";
+
+export default {
+  components: {
+    "chevron-left": chevronLeft
+  },
+  props: ["icon"]
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  box-sizing: border-box;
+  font-family: futura-pt, sans-serif;
+  background: none;
+  border: none;
+  border-radius: 25px;
+  height: 50px;
+  min-width: 150px;
+  padding: 0 15px;
+  background: #095590;
+  color: #fff;
+  font-size: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
