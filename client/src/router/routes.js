5082c9115d144b513c2dd51282c62a06aa506e8c
Peupty <melvillewatford@gmail.com>
2020-03-16 14:00:46 +0100

    added product route

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,3 +1,7 @@
 import Home from "../views/Home.vue";
+import Product from "../components/popup.vue";
 
-export default [{ name: "home", path: "/", component: Home }];
+export default [
+  { name: "home", path: "/", component: Home },
+  { name: "product", path: "/product/:name", component: Product }
+];
