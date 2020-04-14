af22e377b97771753220e98fa153ee9998bea6e9
Peupty <melvillewatford@gmail.com>
2020-04-15 01:12:50 +0200

    create component

---
+++ client/src/components/MobileCard.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="mobileCard col">
+    <header
+      :class="`mobileCard__header mobileCard__header--${type} row`"
+      @click="visible = !visible"
+    >
+      <h3>{{ number }}.&ensp;</h3>
+      <h3>
+        {{ content.header.toUpperCase() }}
+      </h3>
+    </header>
+    <div class="mobileCard__content" v-if="visible">
+      <p class="paragraph">{{ content.text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["content", "number", "type"],
+  data() {
+    return {
+      visible: false
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+* {
+  margin: 0;
+  box-sizing: border-box;
+}
+.mobileCard {
+  color: #fff;
+  margin: 10px auto;
+  width: 280px;
+
+  &__header {
+    cursor: pointer;
+    justify-content: flex-start;
+    border: 2px solid;
+    padding: 10px 20px;
+    border-radius: 8px;
+
+    & h3 {
+      font-size: 16px;
+    }
+
+    &--gray {
+      border-color: var(--gray);
+    }
+    &--lightGray {
+      border-color: var(--lightGray);
+    }
+  }
+}
+.paragraph {
+  margin: 10px;
+  text-align: center;
+  font-size: 18px;
+  font-weight: 500;
+}
+</style>
---
+++ client/src/components/house-mobile-svg.vue
@@ -0,0 +1,49 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="house"
+    viewBox="0 0 363.36 120.336"
+    @click="length"
+  >
+    <path
+      class="a"
+      d="M341.027,168.168H299.976V139.647l-32.332-8.508v43.212L231.106,172.6V150.481l26.933-.377.194,26.173h7.593l.017-45.442-15.6,1.426-.046-1.21,16.637-1.54,20.026,5.546V93L249.58,58.5V146.61l-50.681,2-.308-26.8-19.1,4.005v16.021l31.17-1.786L210.414,90.6l4.66,2.1-.308,47.292,25.879-2.619L240.337,75.6l-32.349,13.71v56.726l39.744-2.051V57.264L202.288,80.833v23.107L175.8,113.053v35.624h15.307v20.149H-22.333"
+      transform="translate(20, -56.441)"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  methods: {
+    length() {
+      const asd = document.getElementsByClassName("a")[0].getTotalLength();
+      console.log(asd);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.a {
+  stroke-dasharray: 1486;
+  stroke-dashoffset: -1486;
+  fill: none;
+  stroke: #fff;
+  stroke-linecap: square;
+  stroke-miterlimit: 5;
+  stroke-width: 1.2px;
+  stroke-miterlimit: 10;
+  animation: animation 3.5s cubic-bezier(0.51, 0.43, 0.19, 0.97) forwards;
+}
+.house {
+  width: 100%;
+  // min-height: 170px;
+  // height: 120px;
+}
+@keyframes animation {
+  to {
+    stroke-dashoffset: 0;
+  }
+}
+</style>
